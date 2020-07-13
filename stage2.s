d18f93267cee1c1f21ba0ba8cb0bd297c86f686a
Gabriela Alexandra Moldovan <gabi_250@live.com>
2020-07-13 22:40:59 +0100

    Print strings byte by byte.

--- stage2.s
+++ stage2.s
@@ -4,6 +4,7 @@ _stage2:
     # Move 0x0 into ES
     mov $0, %ax
     mov %ax, %es
+    mov %ax, %ds
     # Set video mode
     mov $0x0, %ah
     # 80x25
@@ -15,35 +16,28 @@ _stage2:
     # 2 = green
     mov $2, %bl
     int $0x10
-    # Print the message
-    mov msg_len, %cx
-    # DH:DL = row:column
-    xor %dx, %dx
-    # The address of the message is in ES:BP
-    mov $msg, %bp
-    call print_str
+    # The address of the message is in DS:SI
+    mov $msg, %si
+    call print_str2
     call check_a20
-    cmp $1, %ax
-    # DH:DL = row:column
-    xor %dl, %dl
-    mov $1, %dh
-    je a20_message
-    # The message length
-    mov msg_not_enabled_len, %cx
-    # The address of the message is in ES:BP
-    mov $msg_not_enabled, %bp
-    call print_str
+    test %ax, %ax
+    jnz a20_message
+    # The address of the message is in DS:SI
+    mov $msg_not_enabled, %si
+    call print_str2
+    jmp halt
 a20_message:
     # The message length
     mov msg_enabled_len, %cx
-    # The address of the message is in ES:BP
-    mov $msg_enabled, %bp
-    call print_str
+    # The address of the message is in DS:SI
+    mov $msg_enabled, %si
+    call print_str2
+halt:
     hlt
     .include "utils/memory.s"
-msg:                 .ascii "Starting stage 2..."
+msg:                 .asciz "Starting stage 2...\n\r"
 msg_len:             .word (. - msg)
-msg_enabled:         .ascii "A20 enabled"
+msg_enabled:         .asciz "A20 enabled\n\r"
 msg_enabled_len:     .word (. - msg_enabled)
-msg_not_enabled:     .ascii "A20 not enabled"
+msg_not_enabled:     .asciz "A20 not enabled\n\r"
 msg_not_enabled_len: .word (. - msg_not_enabled)
--- utils/bios.s
+++ utils/bios.s
@@ -1,6 +1,7 @@
 .code16
 .text
 .global print_str
+.global print_str2
 
 # Prints a string using bios interrupt 0x10 (AH = 0x13).
 #
@@ -16,4 +17,24 @@ print_str:
     # The colour: 0x14 (1 = blue background, 4 = red text)
     mov $0x14, %bl
     int $0x10
+    ret
+
+# Prints a string one character at a time with bios interrupt 0x10
+# (AH = 0x0E).
+#
+# Arguments:
+#   DS:SI - the address of the string string to print
+print_str2:
+    # The video page.
+    xor %bh, %bh
+    mov $0x0e, %ah
+    # The colour: 0xE0 (E = yellow background, 0 = black text)
+    mov $0x14, %bl
+.Lprint_chars:
+    lodsb
+    or %al, %al
+    jz .Lprint_str2_exit
+    int $0x10
+    jmp .Lprint_chars
+.Lprint_str2_exit:
     ret
